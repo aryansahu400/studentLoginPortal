930d382a8bd08e45607ef11bd140a4319e04abc7
Aryan Sahu <75617873+aryansahu400@users.noreply.github.com>
2021-12-02 10:55:01 +0530

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,24 @@
+
+public class StudentInfo {
+    public String name;
+    public String enrollmentNumber;
+    public String school;
+    public String branch;
+    public String special;
+    public String number;
+    public String email;
+    public String password;
+    
+    StudentInfo(){
+    }
+    StudentInfo(String n,String e,String s, String b, String sp,String nu,String em,String pa ){
+        name=n;
+        enrollmentNumber=e;
+        school=s;
+        branch=b;
+        special=sp;
+        number=nu;
+        email=em;
+        password=pa;
+    }
+}
